237d2660b5d6133f4f766f2b2d28b869c736f0f3
Javkhaa <javkhaa@live.com>
2024-02-23 11:00:34 +0800

    shrimp training with data augmentation

--- underwater_object/yolov8_training.py
+++ underwater_object/yolov8_training.py
@@ -1,12 +1,12 @@
 """
 Training and fine tuning
 """
-from ultralytics import YOLO, settings
-
-settings.update({"datasets_dir": "./data"})
+from ultralytics import YOLO
 
 
-def train_custom_model(epochs: int, verbose: bool = False, resume_training: str = None):
+def train_custom_model(
+    config_yaml: str, epochs: int, verbose: bool = False, resume_training: str = None
+):
     """
     Training YOLO v8
     Fine tune the latest YOLO v8 or resume training of a given model.
@@ -22,17 +22,23 @@ def train_custom_model(epochs: int, verbose: bool = False, resume_training: str 
 
     # Train the model
     model.train(
-        data="./YOLO_configs/openimage_shrimp.yaml",
+        data=config_yaml,
         epochs=epochs,
         device="mps",
-        fraction=0.5,
+        fraction=0.8,
         # imgsz=(480, 848),
         verbose=verbose,
-        weight_decay=0.01,
+        weight_decay=0.05,
         lr0=0.1,
         dropout=0.1,
+        flipud=0.5,
+        fliplr=0.5,
+        scale=0.5,
+        shear=0.5,
         resume=True if resume_training else False,
     )
 
 
-train_custom_model(epochs=100)
+train_custom_model(
+    config_yaml="./YOLO_configs/shrimp_train_v2.yaml", epochs=10, verbose=True
+)
